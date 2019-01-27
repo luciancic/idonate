1ca1d7e1241d9fd917f64c28e375dc06aed564da
Lucian Condrea <luciancondreadev@gmail.com>
2019-01-27 10:07:20 -0500

    Refactor data, change routes

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const data = require('./data');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.json(data))
+app.get('/get', (req, res) => res.json(data))
 
 app.get('/ping', (req, res) => res.send('pong!'))
-
-app.get('/inventory', (req, res) => 
-    res.json(require('./data'))
-)
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
