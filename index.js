1d7295043f9bfeab09144887115f3d8795d052a8
Lucian Condrea <luciancondreadev@gmail.com>
2019-01-26 20:23:51 -0500

    Deploying (#2)
    * Adjust port for Google App Engine
    
    * Add app.yaml
    
    * Add app.yaml runtime
    
    * Add npm scripts
    
    * Add .gcloudignore file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -8,4 +8,4 @@ app.get('/ping', (req, res) => res.send('pong!'))
 
 app.get('/kick', (req, res) => res.send('ass!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
